0f47892b725d18373957ac21cf2449801446acfb
Mi-sho <kamenovv.m@gmail.com>
2025-03-13 19:09:00 +0200

    Add routes for login/register/catalog/createGame

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Catalog from "./components/catalog-page/Catalog";
+import CreateGame from "./components/create-page/CreateGame";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 import "./App.css";
 
@@ -13,6 +17,12 @@ function App() {
       <main id="main-content">
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/catalog" element={<Catalog />}/>
+            <Route path="/create-game" element={<CreateGame />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/register" element={<Register />}/>
+
+
         </Routes>
      
       </main>
