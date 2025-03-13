d423185f8e91fbd0a470eb7360ba5fee9e94a578
Mi-sho <kamenovv.m@gmail.com>
2025-03-13 18:22:20 +0200

    Extract header/home/main components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+
 import "./App.css";
 
 function App() {
   return (
-  <div id="box">
+    <div id="box">
+      <Header />
 
-  </div>
-  )
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
+  );
 }
 
 export default App;
