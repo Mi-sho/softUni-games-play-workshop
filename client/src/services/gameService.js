9e439b75ab74eee6146ddfb5e0da5aee6b4aff9f
Mi-sho <kamenovv.m@gmail.com>
2025-03-14 19:58:00 +0200

    add edit service

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,38 +1,43 @@
-const baseUrl = 'http://localhost:3030/jsonstore/games';
+const baseUrl = "http://localhost:3030/jsonstore/games";
 
 export default {
-    async create(gameData) {
-        const response = await fetch(baseUrl,
-            {method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(gameData)
-            }
-        );
-        const res = await response.json();
+  async create(gameData) {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(gameData),
+    });
+    const res = await response.json();
 
-        return res;
+    return res;
+  },
 
-    },
+  async getAll() {
+    const respone = await fetch(baseUrl);
+    const result = await respone.json();
 
-    async getAll(){
-        const respone = await fetch(baseUrl);
-        const result = await respone.json();
+    const games = Object.values(result);
+    return games;
+  },
 
-        const games = Object.values(result);
-        return games;
-    },
+  async getOne(gameId) {
+    const response = await fetch(`${baseUrl}/${gameId}`);
+    const result = await response.json();
 
-    async getOne(gameId){
-        const response = await fetch(`${baseUrl}/${gameId}`);
-        const result = await response.json();
+    return result;
+  },
 
-        return result;
-
-    }
-
-
-
-
-}
+  async edit(gameId, data) {
+    const response = await fetch(`${baseUrl}/${gameId}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ ...data, _id: gameId }),
+    });
+    const result = await response.json();
+    return result;
+  },
+};
